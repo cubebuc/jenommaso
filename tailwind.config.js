6fcb840d69de916e2696a9c17038391cdcc59eb3
Daniel Martinek <martid12@fit.cvut.cz>
2024-05-11 23:37:28 +0200

    Add fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
     content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                cinzel: ['Cinzel', 'serif'],
+                playfair: ['Playfair Display', 'serif'],
+                avenir: ['Avenir Light', 'serif'],
+            }
+        },
     },
     plugins: [],
 }
