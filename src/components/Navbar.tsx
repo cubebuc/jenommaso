dc7a210ebaef91e092c1ce9544fbfbd154d1fb84
Daniel Martinek <martid12@fit.cvut.cz>
2024-05-19 02:17:53 +0200

    Add icon showing cart item count

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,7 +10,12 @@ function Navbar({ }: Props)
     const [showMenu, setShowMenu] = useState(false)
 
     const { state, dispatch } = useGlobal()
-    const { admin } = state
+    const { admin, cart } = state
+
+    function cartSize()
+    {
+        return Object.values(cart).reduce((acc, val) => acc + val, 0)
+    }
 
     function handleSignOut()
     {
@@ -28,10 +33,11 @@ function Navbar({ }: Props)
                         strokeLinecap='round'
                         stroke='currentColor' />
                 </svg>
+                <span className='absolute size-9 -ms-6 -mt-4 flex justify-center items-center bg-amber-600 rounded-full'>{cartSize()}</span>
             </button>
 
             <header className={`transition-all ${!showMenu && 'invisible opacity-0'} md:visible md:opacity-100 fixed top-0 w-full z-30 flex flex-col md:flex-row justify-between items-center p-3 bg-stone-600 text-white gap-2 md:gap-0`}>
-                <Link className='hidden md:block invisible md:visible w-full md:w-1/3 md:ps-20 text-xl' to='/home'>
+                <Link className='hidden md:block invisible md:visible w-full md:w-1/3 md:ps-16 text-xl' to='/home'>
                     <h1 className='w-fit h-fit hover:scale-105 active:scale-95 transition-transform translate-y-0.5 uppercase'>
                         Jiřího Dobroty
                     </h1>
@@ -46,10 +52,13 @@ function Navbar({ }: Props)
 
                 <hr className='w-full md:hidden' />
 
-                <div className='w-full md:w-1/3 my-3 md:my-0 md:pe-20 flex flex-col md:flex-row justify-center md:justify-end items-center justify-end gap-6 text-2xl md:text-base'>
+                <div className='w-full md:w-1/3 my-3 md:my-0 md:pe-16 flex flex-col md:flex-row justify-center md:justify-end items-center justify-end gap-6 text-2xl md:text-base translate-y-0.5'>
                     {admin && <Link className='text-blue-400 hover:scale-105 active:scale-95 transition-transform' to='/admin'>Admin</Link>}
                     <Link className='hover:scale-105 active:scale-95 transition-transform' to='/profile'>Profil</Link>
-                    <Link className='hover:scale-105 active:scale-95 transition-transform' to='/cart'>Košík</Link>
+                    <Link className='hover:scale-105 active:scale-95 transition-transform flex justify-center' to='/cart'>
+                        Košík
+                        <span className='invisible md:visible absolute size-7 mt-6 flex justify-center items-center bg-amber-600 rounded-full'>{cartSize()}</span>
+                    </Link>
                     <button className='hover:scale-105 active:scale-95 transition-transform' onClick={handleSignOut}>Odhlásit</button>
                 </div>
             </header>
