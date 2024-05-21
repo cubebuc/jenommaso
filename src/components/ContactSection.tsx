40b186998f4e3b36ce8ab9eb98098f7d559c92c7
Daniel Martinek <martid12@fit.cvut.cz>
2024-05-21 02:16:25 +0200

    Fix navbar on medium size screen

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -7,10 +7,10 @@ function ContactSection({ }: Props)
             <hr className='w-20 bg-stone-800 h-0.5 my-5' />
             <h2 className='text-lg mt-1'>JIŘÍHO DOBROTY</h2>
             <div className='mt-8 flex flex-col gap-1'>
-                <p>Jméno: Jiří Vybíral Flekovskej</p>
+                <p>Jméno: Jiří Vybíral</p>
                 <p>Telefon: +420 736 726 806</p>
                 <p>Email: mujemail@gmail.com</p>
-                <p>Učet: </p>
+                <p>Učet: ...</p>
             </div>
         </div>
     )
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -37,7 +37,7 @@ function Navbar({ }: Props)
             </button>
 
             <header className={`transition-all ${!showMenu && 'invisible opacity-0'} md:visible md:opacity-100 fixed top-0 w-full z-30 flex flex-col md:flex-row justify-between items-center p-3 bg-stone-600 text-white gap-2 md:gap-0`}>
-                <Link className='hidden md:block invisible md:visible w-full md:w-1/3 md:ps-16 text-xl' to='/home'>
+                <Link className='hidden md:block invisible md:visible w-full md:w-fit lg:w-1/3 md:ps-[5%] text-xl' to='/home'>
                     <h1 className='w-fit h-fit hover:scale-105 active:scale-95 transition-transform translate-y-0.5 uppercase'>
                         Jiřího Dobroty
                     </h1>
@@ -52,7 +52,7 @@ function Navbar({ }: Props)
 
                 <hr className='w-full md:hidden' />
 
-                <div className='w-full md:w-1/3 my-3 md:my-0 md:pe-16 flex flex-col md:flex-row justify-center md:justify-end items-center justify-end gap-6 text-2xl md:text-base translate-y-0.5'>
+                <div className='w-full md:w-fit lg:w-1/3 my-3 md:my-0 md:pe-[5%] flex flex-col md:flex-row justify-center md:justify-end items-center justify-end gap-6 text-2xl md:text-base translate-y-0.5'>
                     {admin && <Link className='text-blue-400 hover:scale-105 active:scale-95 transition-transform' to='/admin'>Admin</Link>}
                     <Link className='hover:scale-105 active:scale-95 transition-transform' to='/profile'>Profil</Link>
                     <Link className='hover:scale-105 active:scale-95 transition-transform flex justify-center' to='/cart'>
